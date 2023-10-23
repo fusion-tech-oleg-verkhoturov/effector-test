eddd44cefd12c12a268f6444c128fa4527d8f808
Oleg Verkhoturov <oleg.verkhoturov@fusion-tech.pro>
2023-10-23 17:35:49 +0300

    Add loader

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,16 +1,20 @@
 import { useEffect } from "react";
-import { useEvent, useStore } from "effector-react";
-import { Container, List } from "@mui/material";
+import { useEvent, useGate, useStore } from "effector-react";
+import { CircularProgress, Container, List } from "@mui/material";
 
 import Header from "./Header";
 import TodoItem from "../../components/TodoItem";
 import { $todos, updateTodoEvent, deleteTodoEvent, getAllTodos } from "../../store/todos";
+import { getTodosFx } from "../../api";
 
 const Main = () => {
   const todos = useStore($todos);
   const updateTodo = useEvent(updateTodoEvent);
   const deleteTodo = useEvent(deleteTodoEvent);
   const getApiTodos = useEvent(getAllTodos);
+
+  const isLoading = useStore(getTodosFx.pending)
+
 
   useEffect(() => {
     getApiTodos();
@@ -23,11 +27,16 @@ const Main = () => {
       marginTop: '100px'
     }}>
       <Header />
-      <List>
-        {todos.map((todo) => (
-          <TodoItem updateTodo={updateTodo} deleteTodo={deleteTodo} key={todo.id} todo={todo} />
-        ))}
-      </List>
+      {isLoading 
+        ? <CircularProgress />
+        : (
+          <List>
+            {todos.map((todo) => (
+              <TodoItem updateTodo={updateTodo} deleteTodo={deleteTodo} key={todo.id} todo={todo} />
+            ))}
+          </List>
+        ) 
+      }
     </Container>
   )
 }
